02cf418199bbbc1cb401e58c4e5823916765cd1a
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2022-10-04 15:51:04 +0800

    feat:dialog  style

--- src/components/LogoutDialog.js
+++ src/components/LogoutDialog.js
@@ -10,11 +10,13 @@ const LogoutDialog = ({setDialog,setIsLogOut}) =>{
     }
     return(
         <>
+        <div className="dialogModal">
         <div className="dialog">
-        <div className="titleLogout">確定要登出嗎</div>
+        <div className="titleLogout">確定要登出嗎?</div>
         <div className="question">
         <div className="confirmBtn" onClick={logOut}>是</div>
         <div className="cancelBtn" onClick={()=>setDialogs(false)}>否</div>
+        </div>
         </div>
         </div>
         </>
